f1430ddf7b91766b54386054f467a9d88b8a3f59
Arvind <arvind.pravat@gmail.com>
2019-05-08 23:59:29 +0530

    change

--- row_compare.py
+++ row_compare.py
@@ -1,53 +1,30 @@
 import pandas as pd
-import numpy as np
 import openpyxl
-import os
-import xlrd
 
-#df = pd.read_excel('/Users/arvindkumar/Documents/JCI/Input/Book1.xlsx', sheetname='Sheet1')
-
+# Fetching the excel file, Hence please mention the proper excel file path and sheet
 df = pd.read_excel('/Users/arvindkumar/Documents/JCI/Input/Book1.xlsx', sheetname='Sheet1')
-#print(df)
-sdf= df[['Part name', 'Product Name', 'Part Cost','Part Number']]
-print(sdf)
+wb = openpyxl.load_workbook('/Users/arvindkumar/Documents/JCI/Input/Book1.xlsx')
+sheet1 = wb.get_sheet_by_name('Sheet1')
 
-#Count Rows
-#print(len(df.axes[0]))
+# Selecting the columns which are required to be compared
+sdf= df[['Column1', 'Column2', 'Column3','Column4','Column5','Column6','Column7','Column8','Column9','Column10']]
 
-#Count Columns
-#print(len(df.axes[1]))
-
-print(len(sdf.axes[0]))
-
+# Finding number of rows and columns
 totalRows = len(sdf.axes[0])
+totalcols = len(df.axes[1])
 
+colIndex = totalcols + 1
 
-#firstRow = selectedColumns.loc[index,:]
-#list1 = list(column1)
-#column2 = df.loc[2,:]
-#list2 = list(column2)
-
+sheet1.cell(row=1, column= colIndex).value = 'Total Match Rows'
 
 for i in range(0, totalRows):
-    for j in range(0, totalRows):
+
+    for j in range(i+1, totalRows):
 
         if list(sdf.loc[i,:]) == list(sdf.loc[j,:]):
-            print(list(sdf.loc[i, :]))
-            print(list(sdf.loc[j, :]))
-            print("Number was found")
-        else:
-            print(list(sdf.loc[i, :]))
-            print(list(sdf.loc[j, :]))
-            print("Number was not found")
+            if sheet1.cell(row=i + 2, column= colIndex).value is None or  sheet1.cell(row=j+2, column=colIndex).value is None:
+                sheet1.cell(row=i+2, column= colIndex).value = i +1
+                sheet1.cell(row=j+2, column= colIndex).value = i + 1
 
-#if set(list1) & set(list2):
- #   print("Number was found")
-#else:
- #   print("Number not in list")
-
-# Declare a list that is to be converted into a column
-#address = ['Delhi', 'Bangalore', 'Chennai', 'Patna']
-
-# Using 'Address' as the column name
-# and equating it to the list
-#df['Address'] = address
+# Saving excel output file, provide path for save the output file
+wb.save('/Users/arvindkumar/Documents/JCI/Output/OutputFile.xlsx')

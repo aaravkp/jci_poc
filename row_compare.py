1cc8eb5047c8a3eeb659cb8c20813eb05c488bd1
Arvind <arvind.pravat@gmail.com>
2019-05-09 18:32:53 +0530

    bug fix

--- row_compare.py
+++ row_compare.py
@@ -23,20 +23,25 @@ colIndex = totalcols + 1
 
 sheet1.cell(row=1, column=colIndex).value = 'Match Found'
 matchCount = 1
+
+# Row Comparison
 for i in range(0, totalRows):
+
+    isMatched = False
 
     for j in range(i+1, totalRows):
 
-        if list(sdf.loc[i,:]) == list(sdf.loc[j,:]):
-            if sheet1.cell(row=i + 2, column=colIndex).value is None or sheet1.cell(row=j+2, column=colIndex).value is None:
-                if i == 0:
-                    sheet1.cell(row=i + 2, column=colIndex).value = matchCount
-                    sheet1.cell(row=j + 2, column=colIndex).value = matchCount
-                    matchCount = sheet1.cell(row=j + 2, column=colIndex).value
-                else:
-                    sheet1.cell(row=i + 2, column=colIndex).value = matchCount + 1
-                    sheet1.cell(row=j + 2, column=colIndex).value = matchCount + 1
-                    matchCount = sheet1.cell(row=j + 2, column=colIndex).value
+        valuei = sheet1.cell(row=i + 2, column=colIndex).value
+        valuej = sheet1.cell(row=j + 2, column=colIndex).value
+
+        if valuei is None or valuej is None:
+            if list(sdf.loc[i,:]) == list(sdf.loc[j,:]):
+                sheet1.cell(row=i + 2, column=colIndex).value = matchCount
+                sheet1.cell(row=j + 2, column=colIndex).value = matchCount
+                isMatched = True
+
+    if isMatched:
+        matchCount = matchCount + 1
 
 # Saving excel output file, provide path for save the output file
 wb.save(outputFile)

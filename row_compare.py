f063fec713fe443b8610fe8029c703961b16a69b
Arvind <arvind.pravat@gmail.com>
2019-05-09 16:44:20 +0530

    bug fix

--- row_compare.py
+++ row_compare.py
@@ -13,7 +13,7 @@ wb = openpyxl.load_workbook(inputFile)
 sheet1 = wb.get_sheet_by_name(sheetName)
 
 # Selecting the columns which are required to be compared
-sdf= df[['Column1', 'Column2', 'Column3','Column4','Column5','Column6','Column7','Column8','Column9','Column10']]
+sdf = df[['Part name', 'Product Name', 'Part Cost','Part Number']]
 
 # Finding number of rows and columns
 totalRows = len(sdf.axes[0])
@@ -22,15 +22,21 @@ totalcols = len(df.axes[1])
 colIndex = totalcols + 1
 
 sheet1.cell(row=1, column=colIndex).value = 'Match Found'
-
+matchCount = 1
 for i in range(0, totalRows):
 
     for j in range(i+1, totalRows):
 
         if list(sdf.loc[i,:]) == list(sdf.loc[j,:]):
-            if sheet1.cell(row=i + 2, column=colIndex).value is None or  sheet1.cell(row=j+2, column=colIndex).value is None:
-                sheet1.cell(row=i+2, column=colIndex).value = i +1
-                sheet1.cell(row=j+2, column=colIndex).value = i + 1
+            if sheet1.cell(row=i + 2, column=colIndex).value is None or sheet1.cell(row=j+2, column=colIndex).value is None:
+                if i == 0:
+                    sheet1.cell(row=i + 2, column=colIndex).value = matchCount
+                    sheet1.cell(row=j + 2, column=colIndex).value = matchCount
+                    matchCount = sheet1.cell(row=j + 2, column=colIndex).value
+                else:
+                    sheet1.cell(row=i + 2, column=colIndex).value = matchCount + 1
+                    sheet1.cell(row=j + 2, column=colIndex).value = matchCount + 1
+                    matchCount = sheet1.cell(row=j + 2, column=colIndex).value
 
 # Saving excel output file, provide path for save the output file
 wb.save(outputFile)
